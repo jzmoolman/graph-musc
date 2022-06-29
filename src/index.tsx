d15584855f35c3604ab4bbee8f7574f0cfac7c99
JZ Moolman <jzmoolman@scapticom.co.za>
2022-06-28 20:39:52 -0400

    Change neo4j db endpoint to Neo4j auradb

--- src/index.tsx
+++ src/index.tsx
@@ -10,20 +10,27 @@ import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
-import { Neo4jProvider, createDriver } from 'use-neo4j';
+import { Neo4jProvider} from 'use-neo4j';
+import neo4j  from  'neo4j-driver'
 
-const driver = createDriver('neo4j', 'localhost',7687,'neo4j','123456')
+// const driver = createDriver('neo4j', 'localhost',7687,'neo4j','123456')
+const uri = 'neo4j+s://813893ea.databases.neo4j.io'
+const user = 'neo4j'
+const password = 'p6YURX5bFlooyM3vRQizhc0uXY_cSpP_gfgJJQ7v_j8'
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Neo4jProvider driver={driver}>
     
     <App />
     </Neo4jProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
