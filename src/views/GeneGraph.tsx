350bb887c5f5222d530a1fe073494d08efa431c2
JZ Moolman <jzmoolman@scapticom.co.za>
2022-07-05 16:02:28 -0400

    Add FinalVerdict

--- src/views/GeneGraph.tsx
+++ src/views/GeneGraph.tsx
@@ -1,9 +1,11 @@
 import { useContext, useState } from 'react'
 
 import ForceGraph2D  from 'react-force-graph-2d'
-import { DropdownProps } from 'semantic-ui-react'
+import { Checkbox, CheckboxProps, DropdownProps } from 'semantic-ui-react'
 import { Neo4jContext } from 'use-neo4j'
 import {Dropdown} from 'semantic-ui-react'
+import { StringLiteralLike } from 'typescript'
+import { StringifyOptions } from 'querystring'
 
 
 type DataType = {
@@ -13,11 +15,13 @@ type DataType = {
 
 export const GeneGraph = () => {
 
-    console.log('Enter GeneGraph')
-    console.log('===============')
+    console.log('Enter - GeneGraph')
+  
  
     const context = useContext(Neo4jContext), driver = context.driver
 
+    const [finalVerdict, setFinalVerdict] = useState(true)
+    const [filter, setFilter] = useState('')
 
     type dataType = 
     {   nodes: {name:string, nodeColor:string}[] 
@@ -25,37 +29,75 @@ export const GeneGraph = () => {
     }
     const [data, setData] =  useState<dataType>( {nodes: [], links: []} )
     
-    async function dropdownHandler(data: DropdownProps) {
-        console.log('Enter - Dropdown on change')
+    async function finalVerdictHandler(data: CheckboxProps) {
+        console.log('Enter - finalVerdictHandelder')
         console.log(data.value)
-        if (driver == null) {
-            console.log('Driver not loaded')
-            return 
-        }
         if ( !data ) {
             console.log('Data not correct type')
             return 
         }
-    
-        let filter = '';
+
+        const localFinalVerdict : boolean  = data.checked ? data.checked : false
+        console.log('data', data, localFinalVerdict)
+        loadGraph(filter, localFinalVerdict)
+        setFinalVerdict(localFinalVerdict)
+        console.log('Exit - finalVerdictHandelder')        
+    }
+
+    async function dropdownHandler(data: DropdownProps) {
+        console.log('Enter - dropDownHandler')
+        console.log(data.value)
+        if ( !data ) {
+            console.log('Data not correct type')
+            return 
+        }
+        let localFilter: string = '';
         (data.value  as string[]).forEach(value => { 
-            if (filter == '') filter = '['
-            filter = filter + '\'' + value + '\','
+            if (localFilter == '') localFilter = '['
+            localFilter = localFilter + '\'' + value + '\','
         } )
-        if (filter != '') {
-            filter = filter.slice(0, filter.length - 1);
-            filter = filter + ']'
+        if (localFilter != '') {
+            localFilter = localFilter.slice(0, localFilter.length - 1);
+            localFilter = localFilter + ']'
+        }
+        console.log('localFilter =', localFilter, 'filter = ', filter)   
+        loadGraph(localFilter,finalVerdict)
+        setFilter(localFilter)
+        console.log('Exit - dropDownHandler')        
+    }
+
+    async function loadGraph(filter: string, finalVerdict: boolean = true) {
+        console.log('Enter - loadData')
+        if (driver == null) {
+            console.log('Driver not loaded')
+            return 
         }
 
-        let queryG = 'MATCH (g:MGene) RETURN ID(g) as id, g.name as name'
-        let queryO = 'MATCH (o:Organ) RETURN ID(o) as id, o.name as name'
-        let queryR =  'MATCH (g:MGene)-[r]->(o:Organ) RETURN ID(g) as sid,ID(o) as tid, g.name as sname, o.name as tname'
+        let queryG = 'MATCH (g:MGene) RETURN DISTINCT g.name as name'
+        let queryO = `MATCH (g:MGene)--(o:Organ) RETURN DISTINCT o.name as name`
+        let queryR = 'MATCH (g:MGene)-[r]->(o:Organ) RETURN ID(g) as sid,ID(o) as tid, g.name as sname, o.name as tname'
+
+        if (finalVerdict) {
+     
+            queryG = 'MATCH (g:MGene) WHERE g.FinalVerdict = 1 RETURN DISTINCT g.name as name'
+            queryO = `MATCH (g:MGene)--(o:Organ) WHERE g.FinalVerdict = 1 RETURN DISTINCT o.name as name`
+            queryR = 'MATCH (g:MGene)-[r]->(o:Organ) WHERE g.FinalVerdict = 1 RETURN ID(g) as sid, ID(o) as tid, g.name as sname, o.name as tname'
+
+        }
 
         if ( filter !== '' ) {
 
-            queryG = `MATCH (g:MGene) WHERE g.name IN ${filter} RETURN ID(g) as id, g.name as name`
+            queryG = `MATCH (g:MGene) WHERE g.name IN ${filter} RETURN DISTINCT g.name as name`
             queryO = `MATCH (g:MGene)--(o:Organ) WHERE g.name IN ${filter} RETURN DISTINCT o.name as name`
             queryR = `MATCH (g:MGene)-[r]->(o:Organ) WHERE g.name IN ${filter} RETURN ID(g) as sid,ID(o) as tid, g.name as sname, o.name as tname`
+
+            if (finalVerdict) {
+
+                queryG = `MATCH (g:MGene) WHERE g.name IN ${filter} AND g.FinalVerdict = 1 RETURN DISTINCT g.name as name`
+                queryO = `MATCH (g:MGene)--(o:Organ) WHERE g.name IN ${filter}  AND g.FinalVerdict = 1 RETURN DISTINCT o.name as name`
+                queryR = `MATCH (g:MGene)-[r]->(o:Organ) WHERE g.name IN ${filter} AND g.FinalVerdict = 1 RETURN ID(g) as sid,ID(o) as tid, g.name as sname, o.name as tname`
+
+            }
             
         }
         console.log(queryG)
@@ -87,24 +129,26 @@ export const GeneGraph = () => {
             await session.close()
         
         }
-      
     }
 
     type GeneOptionType = {
         value: string
         text: string
     }
-    const [genes, setGenes] = useState<GeneOptionType[]>([{value: '',text:''}])
+    
 
-    const loadGenes = async () => {
-        
-        
-        console.log('Enter - Load Gene data')
-        
+    const [genes, setGenes] = useState<GeneOptionType[]>([])
+
+    let count = 0
+    
+    const loadGenes = async () => {  
+        console.log('Enter - loadGenes', count++)
         if (driver == null) return 
+
+ 
         let session = driver.session()
 
-        let res = await session.run(`MATCH (g:MGene) RETURN g.name as name`)
+        let res = await session.run(`MATCH (g:MGene) RETURN DISTINCT g.name as name ORDER BY name`)
        
 
         let elems = [{value: '',text:''}]
@@ -120,21 +164,60 @@ export const GeneGraph = () => {
         }
         
         setGenes(elems)
-        console.log('Exit - Load Gene data')
+        console.log('Exit - loadGenes')
     }
 
-    if (genes.length < 2 )  loadGenes() 
+    console.log(genes.length)
+    if (genes.length == 0 ) { 
+        loadGenes() 
+    }
 
-            
+    
+    interface NodeObject {
+        name: string
+    }
+          
+    console.log('Exit - GeneGraph')
     return ( 
         <div>
             <div>
-                <Dropdown placeholder='select genes' options={genes} multiple={true} onChange={ (e,data)=>{ console.log(data); dropdownHandler(data) } }></Dropdown>
-                <span> </span>
+                <Dropdown placeholder='Select genes' options={genes} multiple={true} onChange={ (e,data)=>{ console.log(data); dropdownHandler(data) } }></Dropdown>
+                <Dropdown placeholder='Select gender' options={[{text:'Both',value:'Both'},{text:'Male',value:'Male'},{text:'Female',value:'Female'}]} 
+                    selected='true'></Dropdown>
+                <span>  </span>
+                <Checkbox label='Verified' defaultChecked={true} onChange={(e,data)=> { console.log( e); finalVerdictHandler(data)} } />
             </div>
-           
-            <ForceGraph2D graphData={data} nodeId='name' width={800} height={800} backgroundColor='grey' 
-               linkDirectionalArrowRelPos={1} linkDirectionalArrowLength={2} nodeColor='nodeColor' nodeLabel='name'/>       
+            {/* <ForceGraph2D graphData={data} nodeId='name' width={800} height={800} backgroundColor='grey'  */}
+            <div style={{ display:'block',  border: "1px solid gray",}}>
+                <ForceGraph2D 
+                    width={1000}
+                    height={800}
+                    graphData={data} 
+                    nodeId='name'  
+                    backgroundColor='grey' 
+                    linkDirectionalArrowRelPos={1} 
+                    linkDirectionalArrowLength={2} 
+                    nodeColor='nodeColor' 
+                    nodeLabel='name' 
+                    nodeCanvasObjectMode={() => 'after'} 
+                    nodeCanvasObject={(node, ctx, globalScale) => {
+                        
+                        const label = (node as NodeObject).name
+                        const fontSize = 8 /globalScale
+                    
+                        const _x = node.x?node.x:0
+                        const _y = node.y?node.y:0
+                        ctx.font = `${fontSize}px Sans-Serif`;
+                        
+                        ctx.textAlign = 'center';
+                        ctx.textBaseline = 'middle'
+                        ctx.fillStyle = 'black'
+                        ctx.fillText(label, _x, _y + 6)
+                    }}
+                    />
+                
+            </div>       
+
         </div>
     
     )
