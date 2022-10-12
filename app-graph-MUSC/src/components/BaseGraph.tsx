238658a85add4ef7e6e20f3a2d7a4cfe02dfdca9
Armando Daniel Diaz Gonzalez <armandodanieldiazgonzalez@Armandos-MacBook-Pro-2.local>
2022-10-12 05:25:45 -0400

    update background color nccn tables

--- app-graph-MUSC/src/components/BaseGraph.tsx
+++ app-graph-MUSC/src/components/BaseGraph.tsx
@@ -47,8 +47,8 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
+    '&:nth-of-type(n)': {
+        backgroundColor: "rgba(0, 0, 0, 0.1)",
     },
     // hide last border
     '&:last-child td, &:last-child th': {
