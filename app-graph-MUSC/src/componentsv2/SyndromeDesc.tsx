8c5399f3a8adde760f0defecb394682860985c6a
Armando Diaz Gonzalez <arm.dan.diaz.gonzalez@gmail.com>
2023-07-20 07:16:03 -0400

    remove produced by when undefined link card

--- app-graph-MUSC/src/componentsv2/SyndromeDesc.tsx
+++ app-graph-MUSC/src/componentsv2/SyndromeDesc.tsx
@@ -12,24 +12,28 @@ type SyndromeDescProps = {
 
 export const SyndromeDesc = ({syndrome}:SyndromeDescProps) => {
     console.log('----->Debug: SyndromeDesc.gene ', syndrome)
-
+    let produced_by : JSX.Element | undefined
+    
+    if(syndrome.google_drive_link !== undefined){
+        produced_by  = <> <div key={"1"} className="NCCNCard">
+        Produced by AI (ChatGPT)    
+        <br/>
+        Prompts created by Emalie Houk, Kiersten Meeder
+        <br/>
+        ChatGPT queries by Nicole Uzzo, Emalie Houk
+        <br/>
+        Reviewed for accuracy by Nicole Uzzo, Emalie Houk
+        <br/>
+        Please make any suggestions regarding accuracy to keh270@MUSC.edu
+    </div></>
+    }
 
     return (<div> 
         {syndrome?<>
-        <div key={"1"} className="NCCNCard">
-            Produced by AI (ChatGPT)    
-            <br/>
-            Prompts created by Emalie Houk, Kiersten Meeder
-            <br/>
-            ChatGPT queries by Nicole Uzzo, Emalie Houk
-            <br/>
-            Reviewed for accuracy by Nicole Uzzo, Emalie Houk
-            <br/>
-            Please make any suggestions regarding accuracy to keh270@MUSC.edu
-        </div>
+        {produced_by}
         <br/>
         <div>
-            <iframe src='https://drive.google.com/file/d/14cz-yx9XXmbtq-8sjnS9boDutWh3srZs/preview' width='100%' height='800px' frameBorder='0'></iframe>
+            <iframe src={syndrome.google_drive_link} width='100%' height='800px' frameBorder='0'></iframe>
 
         </div>               
         </>
